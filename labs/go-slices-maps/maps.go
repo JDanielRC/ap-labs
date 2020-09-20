fa53986b3cbe2de8251ec6bcb34805578e3b483c
Daniel Rubio <danirubio780@hotmail.com>
2020-09-20 21:21:54 +0000

    Solved slices and maps lab
    Signed-off-by: Daniel Rubio <danirubio780@hotmail.com>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	words := strings.Fields(s)
+	mapping := make(map[string]int)
+	for i := range words {
+		mapping[words[i]]++
+	}
+	return mapping
+}
+
 func main() {
+	wc.Test(WordCount)
 }
