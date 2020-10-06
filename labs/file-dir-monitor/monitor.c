fd30dd99b5686c7787042cfe1700c08642ca74bc
Daniel Rubio <danirubio780@hotmail.com>
2020-10-06 02:14:33 +0000

    Solved file-dir-monitor
    Signed-off-by: Daniel Rubio <danirubio780@hotmail.com>

--- labs/file-dir-monitor/monitor.c
+++ labs/file-dir-monitor/monitor.c
@@ -12,10 +12,9 @@
 int inotifyFd, wd;
 
 //used from: https://linux.die.net/man/3/nftw
-static int display_info(const char *fpath, const struct stat *sb,
-             int tflag, struct FTW *ftwbuf)
+static int display_info(const char *fpath, const struct stat *sb, int tflag, struct FTW *ftwbuf)
 {
-    if(tflag == FTW_D){
+    if(tflag == FTW_D && ftwbuf->level <= 2){ //check for level, either one mentioned or one above
          wd = inotify_add_watch(inotifyFd, fpath, IN_CREATE | IN_DELETE | IN_MOVE);
         if (wd == -1)
             errorf("inotify_add_watch");
@@ -27,33 +26,34 @@ static int display_info(const char *fpath, const struct stat *sb,
 static void displayInotifyEvent(struct inotify_event *i)
 {
     if (i->mask & IN_CREATE){
-        infof("CREATED %s\n",i->name);
+        infof("CREATED FILE WITH NAME: %s\n",i->name);
     }        
     if (i->mask & IN_DELETE){
-        infof("DELETED %s\n",i->name);
+        infof("DELETED FOLLOWING FILE: %s\n",i->name);
     }       
     if (i->mask & IN_MOVED_FROM){
-        infof("RENAMING %s ",i->name);
+        infof("RENAMING FILE WITH NAME: %s ",i->name);
     }   
     if (i->mask & IN_MOVED_TO){
-        infof("TO %s\n", i->name);
+        infof("TO FILE WTIH NAME: %s\n", i->name);
     }
 
 }
 
 int main(int argc, char** argv){
-    int flags = 0;
     char buf[BUF_LEN] __attribute__ ((aligned(8)));
-    ssize_t numRead;
+    struct inotify_event *ocurrence;
+    ssize_t changesRead;
     char *p;
-    struct inotify_event *event;
+    
+    int flags = 0;
 
     if (argc < 2){
         errorf("Wrong number of arguments");
         exit(EXIT_FAILURE);
     }
 
-    inotifyFd = inotify_init();                 /* Create inotify instance */
+    inotifyFd = inotify_init();              
     if (inotifyFd == -1){
         errorf("inotify_init failed");
     }
@@ -63,23 +63,23 @@ int main(int argc, char** argv){
     if (argc > 2 && strchr(argv[2], 'p') != NULL){
         flags |= FTW_PHYS;
     }   
-    if (nftw((argc < 2) ? "." : argv[1], display_info, 20, flags) == -1) {
+    if (nftw(argv[1], display_info, 20, flags) == -1) {
         errorf("nftw");
         exit(EXIT_FAILURE);
     }
     for (;;) { 
-        numRead = read(inotifyFd, buf, BUF_LEN);
-        if (numRead == 0){
+        changesRead = read(inotifyFd, buf, BUF_LEN);
+        if (changesRead == 0){
             errorf("read() from inotify fd returned 0!");
         }
-        if (numRead == -1){
+        if (changesRead == -1){
             errorf("read failed");
         }
-        for (p = buf; p < buf + numRead; ) {
-            event = (struct inotify_event *) p;
-            displayInotifyEvent(event);
+        for (p = buf; p < buf + changesRead; ) {
+            ocurrence = (struct inotify_event *) p;
+            displayInotifyEvent(ocurrence);
 
-            p += sizeof(struct inotify_event) + event->len;
+            p += sizeof(struct inotify_event) + ocurrence->len;
         }
     }
     exit(EXIT_SUCCESS);
